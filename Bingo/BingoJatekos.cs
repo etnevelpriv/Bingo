944eb736387d70cf15ed7a6c1eadacc2bc68d7f1
etnevelpriv <leventerastovits@gmail.com>
2026-03-04 09:27:51 +0100

    Fajl beolvasas kesz. Class keszen van

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal class BingoJatekos
+    {
+        public string Nev { get; set; }
+        public List<int> Szamok { get; set; }
+        public List<int> Talalatok { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nev} - Számok: {string.Join(", ", Szamok)} - Találatok: {string.Join(", ", Talalatok)}";
+        }
+    }
+}
--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            JatekosokBeolvasasa();
+        }
+
+        static void JatekosokBeolvasasa()
+        {
+            List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+            foreach (var file in Directory.GetFiles("../../../Jatekosok", "*.txt"))
+            {
+                {
+                    string nev = Path.GetFileNameWithoutExtension(file);
+                    if (nev != "nevek")
+                    {
+                        string[] sorok = File.ReadAllLines(file);
+                        List<int> szamok = new List<int>();
+                        foreach (var sor in sorok)
+                        {
+                            string[] elemek = sor.Split(';');
+
+                            foreach (var elem in elemek)
+                            {
+                                if (int.TryParse(elem, out int szam))
+                                {
+                                    szamok.Add(szam);
+                                }
+                            }
+                        }
+                        jatekosok.Add(new BingoJatekos { Nev = nev, Szamok = szamok, Talalatok = new List<int>() });
+                    }
+                }
+            }
+            foreach (var jatekos in jatekosok)
+            {
+                Console.WriteLine(jatekos);
+            }
         }
     }
 }
